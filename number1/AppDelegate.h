bbf0f9b98c78ddafb347740c93aeb86fd5e0bdf0
Valeriya Rukavishnikova <Lera@Valeriyas-Mac.local>
2013-10-27 12:03:51 +0400

    Initial Commit

---
+++ number1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  number1
+//
+//  Created by Valeriya Rukavishnikova on 10/27/13.
+//  Copyright (c) 2013 Valeriya Rukavishnikova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
